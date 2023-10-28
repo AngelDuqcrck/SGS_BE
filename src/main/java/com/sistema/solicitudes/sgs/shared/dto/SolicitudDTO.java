9b08ef7cd5cc77f0603556b8fdd17703f1494e15
angeldev07 <angelgaran20@gmail.com>
2023-10-27 23:43:27 -0500

    feat(Solicitud):  Implementado obtener solicitudes por dependencia.

--- src/main/java/com/sistema/solicitudes/sgs/shared/dto/SolicitudDTO.java
+++ src/main/java/com/sistema/solicitudes/sgs/shared/dto/SolicitudDTO.java
@@ -1,10 +1,16 @@
 package com.sistema.solicitudes.sgs.shared.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class SolicitudDTO {
     
     private Integer id;
