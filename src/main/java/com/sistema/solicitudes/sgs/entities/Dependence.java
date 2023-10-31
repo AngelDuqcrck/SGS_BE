aa2881b596e5de7fc79b97ab5c8db8f84990d70b
Angel Yesid Duque <angelyesidducr@ufps.edu.co>
2023-10-30 20:58:51 -0500

    docs: Created the documentation all entities

--- src/main/java/com/sistema/solicitudes/sgs/entities/Dependence.java
+++ src/main/java/com/sistema/solicitudes/sgs/entities/Dependence.java
@@ -14,12 +14,13 @@ import javax.persistence.*;
 @AllArgsConstructor
 @Table(name = "dependencies")
 public class Dependence {
-    
+    //This is the code identifier of each dependence
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Integer id;
 
+    //This atribute is the name of each dependence, that has a lenght of 100 chars
     @Column(name = "description", length = 100)
     private String description;
 }
--- src/main/java/com/sistema/solicitudes/sgs/entities/User.java
+++ src/main/java/com/sistema/solicitudes/sgs/entities/User.java
@@ -1,6 +1,5 @@
 package com.sistema.solicitudes.sgs.entities;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,35 +8,33 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 
-
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Entity
-@Table (name = "user")
+@Table(name = "user")
 public class User {
-
+    // This is the code identifier that is asociated with a user
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
+    // Here, we save the user's first name
     @NotEmpty
     private String firstName;
     @NotEmpty
+    // Here, we save the user's last name
     private String lastName;
-
+    // Here, we save the user's email, this is used in log in
     @NotEmpty
     private String email;
-
+    // This is the password, we use the password to log in, obiously we need a email
     private String password;
-
+    // Here, we save the id of the rol to identify the charge that each user has
     @ManyToOne
-    @JoinColumn(name = "rol_id") 
+    @JoinColumn(name = "rol_id")
     private Rol rol;
-
-
+    //Here, we save the id of the dependence to identify the dependence where user works
     @ManyToOne
     @JoinColumn(name = "dependence_id")
     private Dependence dependence;
